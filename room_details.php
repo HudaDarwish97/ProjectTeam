3eff5085b34305362771d481e7ba327d9dcc51f8
Narjis Rasti <146778166+NarjesRasti@users.noreply.github.com>
2024-12-04 21:26:37 +0300

    Update room_details.php

--- room_details.php
+++ room_details.php
@@ -71,7 +71,7 @@ if ($result->num_rows > 0) {
         </div>
 
         <div class="book-button">
-    <a href="booking.php?room_id=<?php echo $room['room_id']; ?>" class="btn btn-primary">Book This Room</a>
+    <a href="booking.html?room_id=<?php echo $room['room_id']; ?>" class="btn btn-primary">Book This Room</a>
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
