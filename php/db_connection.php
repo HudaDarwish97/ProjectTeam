aaa5cc9b13ee8ce83b4481d7d3fb7081fe0984d7
Noor <139276872+Noorr9@users.noreply.github.com>
2024-12-08 21:57:44 +0300

    Update db_connection.php

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dsn = "mysql:host=localhost;port=4306;dbname=room_booking;charset=utf8mb4";
+    $dsn = "mysql:host=localhost;port=3306;dbname=room_booking;charset=utf8mb4";
     $conn = new PDO($dsn, "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
