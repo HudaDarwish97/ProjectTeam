e0f02cacbe347cd3e48381f3b0a5a7059225de65
Narjis <146778166+NarjesRasti@users.noreply.github.com>
2024-12-09 17:45:51 +0300

    css

--- views/room_details.php
+++ views/room_details.php
@@ -27,6 +27,7 @@ if (!$room) {
     <link  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+        
 
         <link rel="stylesheet" href="../css/room_details.css">
 </head>
