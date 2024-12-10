e8442ed2a4cde04cf84d981f1c9ad0d7cda043ef
Noor <139276872+Noorr9@users.noreply.github.com>
2024-12-10 17:19:53 +0300

    change no port

--- php/register.php
+++ php/register.php
@@ -7,7 +7,7 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 // Include database connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME,Port);
 
 // Check connection
 if ($conn->connect_error) {
