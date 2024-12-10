b23474532a6951be17760e8f56b65cf724757065
Noor <139276872+Noorr9@users.noreply.github.com>
2024-12-10 17:19:29 +0300

    change port no

--- php/config.php
+++ php/config.php
@@ -4,6 +4,7 @@ define('BASE_URL', '/ProjectTeam');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
+define('Port', '3306');
 define('DB_NAME', 'room_booking');
 
 ?>
