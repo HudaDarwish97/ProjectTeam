12f963fc6bf6e1b93192d3035ae7c2398eb9e77d
202102792 <154744412+202102792@users.noreply.github.com>
2024-12-11 23:44:12 +0300

    linking firstask.css style sheet

--- php/MyBookings.php
+++ php/MyBookings.php
@@ -34,6 +34,7 @@ $userBookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/firstask.css">
     <title>My Bookings</title>
     <style>
         body {
